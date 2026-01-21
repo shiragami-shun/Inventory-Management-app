44765269f407c9f6ca5d4b7018d03b261cceb74b
shiragami-shun <shun.shiragami0904@gmail.com>
2026-01-21 11:30:02 +0900

    homeに戻る機能の追加

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from myapp import views
 
 urlpatterns = [
     path('sales/', views.dashboard, name='dashboard'),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
 ]
